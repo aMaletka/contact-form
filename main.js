f4f766485e05cbb8f43b3ad7371a41b25ddc5bf1
Anna Małetka <maletka.a@icloud.com>
2021-09-26 19:04:35 +0200

    add subscribe newsletter

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ const firstName = document.getElementById("name");
 const email = document.getElementById("email");
 const message = document.getElementById("message");
 const submit = document.getElementById("submit");
+const subscribe = document.getElementById("subscribe");
 const contactForm = document.getElementById("contactForm");
 const answerYes = document.querySelector("#yes");
+const answerNo = document.querySelector("#no");
 const phone = document.getElementById("phone");
 const chooseAnswer = document.querySelector(".chooseAnswer");
-
+const signUp = document.getElementById("signUp");
 
 const users = [];
+const emails = [];
 
 const sendForm = () => {
    const newUser = {
@@ -17,10 +20,20 @@ const sendForm = () => {
        message: message.value,
    }
    users.push(newUser)
-   
+   firstName.value = "";
+   email.value = "";
+   message.value = "";
+   answerYes.checked = false;
+   answerNo.checked = false;
+   phone.value = "";
 }
-
-
+const signUpToNewsletter = () => {
+    const newSubscriber = {
+        emailAddress: subscribe.value
+    }
+    emails.push(newSubscriber);
+    subscribe.value = "";
+}
 
 const showField = () => {
     if(answerYes.checked) {
@@ -30,16 +43,14 @@ const showField = () => {
     }
 }
 
-
-
-
-
 contactForm.addEventListener("submit", e => {
     e.preventDefault();
     sendForm()
 })
 
 chooseAnswer.addEventListener("change", showField);
+signUp.addEventListener("click", signUpToNewsletter);
+
 
 
 
